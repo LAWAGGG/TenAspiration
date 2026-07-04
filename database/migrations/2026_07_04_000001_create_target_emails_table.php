1f7556b1936772c5c6e622ad69b28be7e27b8f94
LAWAGGG <ahmadfagih.arrifai@gmail.com>
2026-07-04 19:31:41 +0700

    fixing gitignore

--- database/migrations/2026_07_04_000001_create_target_emails_table.php
+++ database/migrations/2026_07_04_000001_create_target_emails_table.php
@@ -15,14 +15,6 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
-
-        TargetEmail::insert([
-            ['email' => 'ahmadfagih.arrifai@gmail.com', 'is_active' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'yunitakamali72@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'mujahidrobbanisholahudin@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'desita1412@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'sayutiazwarmi67@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-        ]);
     }
 
     public function down(): void
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //gabisa loe heheh
+        User::insert([
+            [
+                "name" => "admin",
+                "role" => "admin",
+                "password" => bcrypt("mpk58")
+            ]
+        ]);
+
+        User::insert([
+            [
+                "name" => "wakil",
+                "role" => "wakil",
+                "password" => bcrypt("wakilsmkn10")
+            ]
+        ]);
+
+        TargetEmail::insert([
+            ['email' => 'ahmadfagih.arrifai@gmail.com', 'is_active' => true, 'created_at' => now(), 'updated_at' => now()],
+            ['email' => 'yunitakamali72@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
+            ['email' => 'mujahidrobbanisholahudin@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
+            ['email' => 'desita1412@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
+            ['email' => 'sayutiazwarmi67@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
+        ]);
     }
 }
