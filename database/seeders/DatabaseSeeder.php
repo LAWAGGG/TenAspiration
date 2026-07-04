6bbd2e06a2aa57e44be792cd8ab6c1bab398f877
LAWAGGG <ahmadfagih.arrifai@gmail.com>
2026-07-04 19:18:56 +0700

    fixing data leak

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,28 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::insert([
-            [
-                "name" => "admin",
-                "role" => "admin",
-                "password" => bcrypt("mpk58")
-            ]
-        ]);
-
-        User::insert([
-            [
-                "name" => "wakil",
-                "role" => "wakil",
-                "password" => bcrypt("wakilsmkn10")
-            ]
-        ]);
-
-        TargetEmail::insert([
-            ['email' => 'ahmadfagih.arrifai@gmail.com', 'is_active' => true, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'yunitakamali72@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'mujahidrobbanisholahudin@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'desita1412@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-            ['email' => 'sayutiazwarmi67@gmail.com', 'is_active' => false, 'created_at' => now(), 'updated_at' => now()],
-        ]);
+        //gabisa loe heheh
     }
 }
